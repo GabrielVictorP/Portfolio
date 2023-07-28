b6a8d215746d99a50914f5c6a5f22efa5429a999
Gabriel Victor <133161909+GabrielVictorP@users.noreply.github.com>
2023-07-28 17:14:27 -0300

    Projeto final
    Esse será o último commit em que irei fazer no Portfólio 1.0
    Pretendo voltar com o Portfólio 2.0 assim que eu tiver um conhecimento maior

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ ScrollReveal().reveal('.home-content h1', { origin: 'left'});
 ScrollReveal().reveal('home-content p, .about-content ', { origin: 'right'});
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Desenvolvedor front end'],
+    strings: ['Desenvolvedor front-end'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
